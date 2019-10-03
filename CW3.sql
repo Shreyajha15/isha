dd72feaec7cb965209318510e504d9c585923f76
alekhya-ravilla <54561809+alekhya-ravilla@users.noreply.github.com>
2019-10-02 19:26:00 -0500

    Add files via upload

--- CW3.sql
+++ CW3.sql
@@ -3,23 +3,25 @@
 USE schoolschedulingexample;
 SELECT StfLastName AS 'Last Name', StfFirstName AS 'First Name', Salary
 FROM staff
-WHERE Salary >= 40000 AND Salary <= 50000
-ORDER BY StfLastName, StfFirstName ASC;
+WHERE Salary BETWEEN 40000 AND 50000
+ORDER BY Salary, StfLastName, StfFirstName ASC;
 
 -- Show me a list of students whose last name is ‘Kennedy’ or who live in Seattle
 
 USE schoolschedulingexample;
 SELECT StudLastName AS 'Student Last Name', StudFirstName AS 'Student First Name', StudCity AS 'City'
 FROM students
-WHERE StudLastName LIKE 'kennedy' OR StudCity LIKE 'seattle'
+WHERE StudLastName LIKE '%kennedy%' OR StudCity LIKE 'seattle'
 ORDER BY StudLastName, StudFirstName ASC;
 
 -- List the ID numbers of the teams that won one or more of the first ten matches in Game 3
 
 USE bowlingleagueexample;
-SELECT WinningTeamID AS 'Team ID', GameNumber, MatchID
+SELECT MatchID, GameNumber, WinningTeamID AS 'Team ID'
 FROM match_games
-WHERE GameNumber = 3 AND MatchID <= 10;
+WHERE GameNumber = 3
+ORDER BY MatchID
+LIMIT 10;
 
 -- List the bowlers in teams 3, 4, and 5 whose last names begin with the letter ‘H’
 
@@ -47,27 +49,37 @@ WHERE IngredientClassID = 2 AND IngredientName NOT LIKE '%chicken%';
 -- Provide a list of classes along with the associated faculty for courses offered by tenured faculty
 
 USE schoolschedulingexample;
-SELECT c1.ClassID, subj1.SubjectName, s1.StfLastname, s1.StfFirstName, f1.Tenured
-FROM classes c1, faculty f1, faculty_subjects fs1, staff s1, subjects subj1
-WHERE f1.Tenured = 1 AND f1.StaffID = fs1.StaffID AND fs1.SubjectID = c1.SubjectID
- AND f1.StaffID = s1.StaffID AND fs1.SubjectID = subj1.SubjectID
-ORDER BY subj1.SubjectName, s1.StfLastname, s1.StfFirstName;
+SELECT DISTINCT subj1.SubjectCode, subj1.SubjectName, CONCAT(s1.StfFirstName, " ", s1.StfLastname)
+FROM faculty f1 JOIN staff s1 ON f1.StaffID = s1.StaffID
+	 JOIN faculty_classes fc1 ON f1.StaffID = fc1.StaffID
+     JOIN classes c1 ON c1.ClassID = fc1.ClassID
+	 JOIN subjects subj1 ON c1.SubjectID = subj1.SubjectID
+WHERE Tenured = 1
+ORDER BY 1, 3;
 
 -- Provide a list of all engagements along with the names of Agent and Entertainers who are from Seattle
 
 USE entertainmentagencyexample;
 SELECT e1.EngagementNumber, ent1.EntStageName, ent1.EntCity, a1.AgtLastName, a1.AgtFirstName, a1.AgtCity
-FROM engagements e1, agents a1, entertainers ent1
-WHERE (e1.AgentID = a1.AgentID AND a1.AgtCity LIKE 'seattle') AND (e1.EntertainerID = ent1.EntertainerID AND ent1.EntCity LIKE 'seattle')
+FROM engagements e1 JOIN agents a1 ON e1.AgentID = a1.AgentID
+	JOIN entertainers ent1 ON e1.EntertainerID = ent1.EntertainerID
+WHERE EntCity LIKE 'seattle'
 ORDER BY ent1.EntStageName, a1.AgtLastName, a1.AgtFirstName;
 
 -- Provide a list of recipes who have no eggs or meat in their ingredients
 
 USE recipesexample;
-SELECT * FROM ingredients;
 SELECT r1.RecipeID, r1.RecipeTitle, i1.IngredientName
-FROM recipes r1, ingredients i1, recipe_ingredients ri1
-WHERE i1.IngredientClassID != 2 AND i1.IngredientID != 62 AND i1.IngredientID = ri1.IngredientID AND ri1.RecipeID = r1.RecipeID
-ORDER BY r1.RecipeID;
+FROM recipes r1 JOIN recipe_ingredients ri1 ON ri1.RecipeID = r1.RecipeID
+ JOIN ingredients i1 ON ri1.IngredientID = i1.IngredientID
+ JOIN ingredient_classes ic1 ON i1.IngredientClassID = ic1.IngredientClassID
+WHERE IngredientClassDescription <> 'meat'
+ AND IngredientName NOT REGEXP '^*eggs*|bacon*|beef*|chicken*|pork*|steak*'
+ORDER BY RecipeID;
 
 -- Provide a list of teams along with the names of the team captains
+
+USE bowlingleagueexample;
+SELECT t1.TeamName, b1.BowlerLastName, b1.BowlerFirstName
+FROM teams t1 JOIN bowlers b1 ON t1.CaptainID = b1.BowlerID
+WHERE CaptainID = BowlerID;
