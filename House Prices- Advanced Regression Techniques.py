c9bdfa271ddb13e3661c6c9f5672046404624438
alekhya-ravilla <54561809+alekhya-ravilla@users.noreply.github.com>
2019-11-08 11:16:03 -0600

    Add files via upload

--- House Prices- Advanced Regression Techniques.py
+++ House Prices- Advanced Regression Techniques.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[1]:
 
 
 import numpy as np # linear algebra
@@ -12,13 +12,13 @@ train_data = pd.read_csv('./input/house-prices-advanced-regression-techniques/tr
 
 data  = train_data.copy()
 
-# data.info()
+data.info()
 
-# data.describe()
+data.describe()
 
-# data.head()
+data.head()
 
-# any(data.isnull())
+any(data.isnull())
 
 # isnull result is showing us that columnns Alley(91), FireplaceQu(770), PoolQC(7), Fence(281), MiscFeature(54)
 # have missing data ranging from 50%-96%
@@ -26,9 +26,9 @@ data  = train_data.copy()
 # Hence droping columns Alley, PoolQC, Fence, MiscFeature
 data = data.drop(['Alley', 'PoolQC', 'Fence', 'MiscFeature'], axis = 1) 
 
-# sorted(data)
-# Must re-look into FireplaceQu to decide whether to drop/ impute
-data.FireplaceQu.unique()
+
+# In[2]:
+
 
 # Impute LotFrontageMedian
 mLotFrontageMedian = data.LotFrontage.median(skipna=True)
@@ -51,14 +51,8 @@ print("Has any Missing Values? \n")
 for col in num_cols:
     print(col, ": ", any(num_data[col].isnull()))
 
-
-# In[2]:
-
-
 data.profile_report()
-
-
-# In[30]:
+# In[3]:
 
 
 # As per above Correlation matrix the following list of columns are not correlated with Target variable: SalePrice
@@ -69,10 +63,10 @@ data = data.drop(['3SsnPorch', 'MiscVal', 'MoSold', 'YrSold', 'BsmtHalfBath', 'B
 list(data)
 
 
-# In[52]:
+# In[4]:
 
 
-# Categorical Values
+# Imputing Categorical Values
 
 cat_data = data.select_dtypes(include=['object'])
 
@@ -83,10 +77,49 @@ cat_cols = list(cat_data)
 print("Has any Missing Values? \n")
 for col in cat_cols:
     print(col, ": ", any(cat_data[col].isnull()))
+    
 
 
-# In[53]:
+# In[5]:
 
+
+# Import label encoder 
+from sklearn import preprocessing 
+  
+# label_encoder object knows how to understand word labels. 
+label_encoder = preprocessing.LabelEncoder() 
+  
+    
+# Encode labels for all the nominal-value columns
+
+columns = [
+        'MSZoning', 'Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+        'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl',
+        'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation', 'BsmtExposure',
+        'BsmtFinType1', 'BsmtFinType2', 'Heating', 'CentralAir',
+        'Electrical', 'Functional', 'GarageType', 'GarageFinish',
+        'PavedDrive', 'SaleType', 'SaleCondition']
+
+data_nominal = pd.DataFrame(columns = columns)
+
+for col in columns:
+    # print(cat_data[col].unique())
+    data_nominal[col] = label_encoder.fit_transform(cat_data[col]) 
+  
+list(cat_data)
+
+
+data = data.drop([
+        'MSZoning', 'Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+        'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl',
+        'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation', 'BsmtExposure',
+        'BsmtFinType1', 'BsmtFinType2', 'Heating', 'CentralAir',
+        'Electrical', 'Functional', 'GarageType', 'GarageFinish',
+        'PavedDrive', 'SaleType', 'SaleCondition'], axis = 1)
+
+data = pd.concat([data, data_nominal], axis = 1)
+
+list(data)
 
 
 # ONE HOT ENCODING
@@ -115,8 +148,7 @@ data = pd.concat([data, enc_cat_data], axis = 1)
 
 list(data)
 
-
-# In[54]:
+# In[6]:
 
 
 ordinalColNames = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual', 'FireplaceQu', 'GarageQual', 'GarageCond']
@@ -134,7 +166,49 @@ scale_mapper = {'Ex': 5,
 # Map feature values to scale
 for col in ordinalColNames :
     data[col] = data[col].replace(scale_mapper)
-    print(data[col])
+    #print(data[col])
+    
+len(list(data))
+
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+# Scaling the data using Standardization
+
+from sklearn.preprocessing import StandardScaler
+
+# Initialise the Scaler 
+scaler = StandardScaler()
+  
+# To scale data 
+data_std = pd.DataFrame(scaler.fit_transform(data), columns = list(data))
+
+data_std.head()
+
+
+# In[14]:
+
+
+# Train Test 
+
+y = data.SalePrice
+
+X = data_std.drop(['SalePrice'], axis = 1) 
+
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state = 42)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
 
 
 # In[ ]:
@@ -146,5 +220,7 @@ for col in ordinalColNames :
 # In[ ]:
 
 
-
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)
 
